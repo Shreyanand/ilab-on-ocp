23b28d84d93e4b53085529eb51ee79c7d7f792eb
Shreyanand <shanand@redhat.com>
2024-09-11 18:13:10 -0400

    Add parameters to pytorch job
    Signed-off-by: Shreyanand <shanand@redhat.com>

--- training/faked/components.py
+++ training/faked/components.py
@@ -7,7 +7,13 @@ from typing import NamedTuple
 
 @dsl.component(base_image="registry.access.redhat.com/ubi9/python-311:latest")
 def pytorchjob_manifest_op(
-    name_suffix: str, pvc_name: str
+    name_suffix: str,
+    pvc_name: str,
+    num_proc_per_node: int,
+    nnodes: int,
+    num_workers: int,
+    base_image: str,
+    data_path: str                      
 ) -> NamedTuple("outputs", manifest=str, name=str):
     Outputs = NamedTuple("outputs", manifest=str, name=str)
     return Outputs("", "")
